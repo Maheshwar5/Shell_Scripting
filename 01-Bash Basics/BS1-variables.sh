24a2dc2d6ff5eca17b6a8732274993156c47078f
Maheshwar5 <veerasuri.maheshwar@outlook.com>
2023-07-30 16:09:15 +0530

    Working on basic scripts

--- 01-Bash Basics/BS1-variables.sh
+++ 01-Bash Basics/BS1-variables.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "Hi"
 day=$(date +%u)
+# -eq is a equal symbol, here we used it to comapre today's day with Monday. 1-Monday,2-Tuesday,..7-Sunday. 
 if [ $day -eq 1 ]; then
     echo "Today is Monday, login to System and check Mails"
 else
-    echo "Today is not Monday"
+    echo "Today is not Monday, Have a great day!"
 fi
 
